c7cf7574ae734e86014a9f4104e93104f33930de
Arwa <arwa.it15@gmail.com>
2017-07-05 08:46:50 -0500

    mvp avhieved

--- javascripts/dynamicDOM.js
+++ javascripts/dynamicDOM.js
@@ -1,10 +1,42 @@
 var SandwichMaker = (function(maker) {
-	SandwichMaker.loadMeat();
-maker.logIt = function()
-{
-	console.log('hi')
-	console.log(SandwichMaker.addMeat());
-}
+	var meat = SandwichMaker.addMeat();
+	var cheese = SandwichMaker.addCheese();
+	var veggies = SandwichMaker.addVeggies();
+	var condiment = SandwichMaker.addCondiments();
+	var bread = SandwichMaker.addBread();
+
+	var optionsArray = [meat, cheese, veggies, condiment, bread];
+	optionsArray.forEach(function(item)
+	{
+		createDom(item);
+	})
+	
+	function createDom(data)
+	{
+		let keyName = Object.keys(data);
+		let mainDiv = document.getElementById('options');
+		let subDiv = document.createElement('div');
+		subDiv.setAttribute('id',`subDiv-${keyName}`);
+		mainDiv.appendChild(subDiv);
+		let labels = document.createElement('label');
+		let valueKey = Object.values(data)[0];
+		labels.innerHTML = `<b>${keyName}:<b>&nbsp`;
+		subDiv.appendChild(labels)
+		for(let i in valueKey)
+		{
+			let inputTag = document.createElement('input');
+			inputTag.setAttribute('type','checkbox')
+			inputTag.setAttribute('name', keyName);
+			inputTag.setAttribute('id', i);
+			inputTag.setAttribute('value', valueKey[i]);
+			subDiv.appendChild(inputTag);
+			let label = document.createElement('label');
+			label.setAttribute('for',i)
+			label.innerHTML = i;
+			subDiv.appendChild(label);
+		}
+
+	}
   return maker;
 
 })(SandwichMaker || {});
--- javascripts/veggies.js
+++ javascripts/veggies.js
@@ -2,24 +2,19 @@
 var SandwichMaker = (function(maker) {
 
   // Private variable to store the different meat prices
-  var veggiePrices = [];
-  // SandwichMaker.optionList = SandwichMaker.optionList || [];
-	maker.loadVeggies = function()
-	{
-	var req = new XMLHttpRequest();
-		req.open("GET", "Data/veggies.json");
-		req.send();
-		req.addEventListener('load', function()
-		{
-			var veggiePrices = JSON.parse(event.target.responseText).veggies[0];		
-			maker.loadCondiments();
-		});
-	}
-  // Augment the original object with another method
+  var veggiePrices = {
+  	"Veggies":
+  		{
+		"bellpepper": 1.00,
+		"tomato": 0.90,
+		"cucumber": 0.80,
+		"lettuce": 0.70,
+		"onions": 0.85
+		}
+	};
   maker.addVeggies = function() {
-    return veggiePrices[0];
+    return veggiePrices;
   };
-
-  // Return the new, augmented object with the new method on it
+  
   return maker;
 })(SandwichMaker || {});
